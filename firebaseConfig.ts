05610a52a751115e27c4ba3690b4f26e4f83ee28
Tanzeel-khan <ktanzeel184@gmail.com>
2024-11-19 17:27:10 +0500

    refactor and update firebase config

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore } from "firebase/firestore"
-import {getAuth , GithubAuthProvider} from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GithubAuthProvider } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-qqsJTlxTq65K2ToL6FQdYItwtA2S1nI",
-  authDomain: "ehsaandraw.firebaseapp.com",
-  databaseURL: "https://ehsaandraw-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ehsaandraw",
-  storageBucket: "ehsaandraw.firebasestorage.app",
-  messagingSenderId: "324689529899",
-  appId: "1:324689529899:web:1182f42f688d6f4823ae6e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -22,5 +22,5 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GithubAuthProvider();
 
-export {auth,provider}
-export const database = getFirestore(app)
+export { auth, provider };
+export const database = getFirestore(app);
